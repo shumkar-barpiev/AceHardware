8dea73653b13f4bbcd66a52c22150e8777ce29f1
Shumkar-Barpiev <barpievsh@gmail.com>
2023-01-21 13:47:49 +0600

    creating category model

---
+++ AceHardware001/Models/CategoryModel.swift
@@ -0,0 +1,14 @@
+//
+//  CategoryModel.swift
+//  AceHardware001
+//
+//  Created by User on 21/1/23.
+//
+
+import Foundation
+
+struct Category: Codable{
+    let id: Int
+    let categoryName: String
+    let categoryImageName: String
+}
