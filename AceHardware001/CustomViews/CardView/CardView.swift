57eb01ebefb31032269ddb9c9b84929002309a82
Shumkar-Barpiev <barpievsh@gmail.com>
2023-01-31 23:14:25 +0600

    creating card view

---
+++ AceHardware001/CustomViews/CardView/CardView.swift
@@ -0,0 +1,27 @@
+//
+//  CartView.swift
+//  AceHardware001
+//
+//  Created by User on 31/1/23.
+//
+
+import UIKit
+
+class CardView: UIView{
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        initialSetup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        initialSetup()
+    }
+    
+    private func initialSetup(){
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = .zero
+        layer.cornerRadius = 15
+        layer.shadowOpacity = 0.5
+    }
+}
