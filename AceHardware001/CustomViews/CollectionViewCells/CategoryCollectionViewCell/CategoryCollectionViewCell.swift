e7186f304513b1b75c071bb21d95ca54796cd8db
Shumkar-Barpiev <barpievsh@gmail.com>
2023-01-31 21:11:39 +0600

    creating category collection view cell

---
+++ AceHardware001/CustomViews/CollectionViewCells/CategoryCollectionViewCell/CategoryCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryCollectionViewCell.swift
+//  AceHardware001
+//
+//  Created by User on 31/1/23.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
