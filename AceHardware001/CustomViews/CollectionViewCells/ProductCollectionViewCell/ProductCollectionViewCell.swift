6365aa5214232283a5d361dce3e6b6f343f1e79f
Shumkar-Barpiev <barpievsh@gmail.com>
2023-02-02 13:36:37 +0600

    implementing product collection view cell setup

--- AceHardware001/CustomViews/CollectionViewCells/ProductCollectionViewCell/ProductCollectionViewCell.swift
+++ AceHardware001/CustomViews/CollectionViewCells/ProductCollectionViewCell/ProductCollectionViewCell.swift
@@ -9,5 +9,17 @@ import UIKit
 
 class ProductCollectionViewCell: UICollectionViewCell {
 
-
+    @IBOutlet weak var productImageView: UIImageView!
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var productPriceLabel: UILabel!
+    
+    static let identifier = String(describing: ProductCollectionViewCell.self)
+    
+    func setUp(product: Product){
+        productImageView.image = UIImage(named: product.productImageName)
+        productNameLabel.text = product.productName
+        productPriceLabel.text = "$ \(product.price)"
+    }
+    
+    
 }
