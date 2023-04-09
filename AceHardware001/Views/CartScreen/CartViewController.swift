ae284f09a5a299260c898a4eb6976306c1fa69a9
shumkar-barpiev <barpievsh@gmail.com>
2023-04-09 13:05:26 +0600

    implementing delete from cart functionality

--- AceHardware001/Views/CartScreen/CartViewController.swift
+++ AceHardware001/Views/CartScreen/CartViewController.swift
@@ -199,5 +199,71 @@ extension CartViewController: UITableViewDelegate, UITableViewDataSource{
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(cartProducts[indexPath.row].id)
+        print()
+        
+        let alertController = UIAlertController(title: "Иш-аракеттер тизмеси.", message: "", preferredStyle: .alert)
+        
+        let alertDeleteAction = UIAlertAction(title: "Корзинадан өчүрүү", style: .default) { _ in
+            self.deleteFromCart(self.userCart[0].id, self.cartProducts[indexPath.row].id)
+            
+            let alertController = UIAlertController(title: "Товар ийгиликтүү өчүрүлдү!!!", message: .none, preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "Түшүнүктүү", style: .default) { _ in
+                let controller = self.storyboard?.instantiateViewController(withIdentifier: "MainTabbarViewController") as! MainTabbarViewController
+                controller.user = self.user
+                controller.modalPresentationStyle = .fullScreen
+                self.present(controller, animated: false, completion: nil)
+            }
+            
+            alertController.addAction(alertAction)
+            self.present(alertController, animated: true, completion: nil)
+            
+        }
+        let alertCancelAction = UIAlertAction(title: "Айныдым", style: .cancel) { _ in
+            
+        }
+        
+        alertController.addAction(alertDeleteAction)
+        alertController.addAction(alertCancelAction)
+        present(alertController, animated: true, completion: nil)
+        
+    }
+    
+    
+    //    MARK:  delete from cart
+    public func deleteFromCart(_ cartID: Int, _ productID: Int){
+        
+        guard let url = URL(string:"http://localhost/BackendAPIphp/api/customerCartAPI.php" ) else{
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        
+        // method body, headers
+        request.httpMethod = "DELETE"
+        
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        let body: [String: AnyHashable] = [
+            "cartId": cartID,
+            "productId": productID
+        ]
+        request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: .fragmentsAllowed)
+        
+        //make request
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else{
+                return
+            }
+            // Convert HTTP Response Data to a String
+            if let dataString = String(data: data, encoding: .utf8) {
+                print("Response data string:\n \(dataString)")
+            }else{
+                print("something wrong")
+            }
+        }
+        
+        task.resume()
+    }
     
 }
